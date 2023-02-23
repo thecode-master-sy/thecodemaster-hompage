4b83affb27a6343787ead995c2f35530ba20da17
thecode-master-sy <thecodemasster@gmail.com>
2023-02-23 21:12:46 +0100

    made little changes, yo!

--- app/works/[work]/page.tsx
+++ app/works/[work]/page.tsx
@@ -1,3 +1,16 @@
-export default function Page({ params }: { params: { slug: string } }) {
-  return <h1>Page</h1>;
+import { StyledContainer, Title } from "@/components/styles/css.styled";
+import { workData } from "@/data/data";
+
+export default function Page({ params }: { params: { work: string } }) {
+  const { work } = params;
+
+  return (
+    <StyledContainer>
+      <div>
+        <Title>Work</Title>
+        <span></span>
+        <span>{work}</span>
+      </div>
+    </StyledContainer>
+  );
 }
