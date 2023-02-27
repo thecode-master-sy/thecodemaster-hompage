0fd44fd1559011d905b1ba936c2282bb63f66eec
thecode-master-sy <thecodemasster@gmail.com>
2023-02-27 19:57:19 +0100

    testing the theme switch problem on firefox

--- components/styles/theme/themeProvider.tsx
+++ components/styles/theme/themeProvider.tsx
@@ -20,14 +20,7 @@ export const useTheme = () => {
 const ThemeContextProvider = ({ children }: { children: React.ReactNode }) => {
   const [mode, setMode] = useState<string>("light");
 
-  useEffect(() => {
-    let Dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-    if (Dark) {
-      setMode("dark");
-    }
-  }, []);
-
+ 
   const updateMode = () => {
     if (mode === "light") {
       setMode("dark");
