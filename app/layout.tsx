37801ef98b4be65d8f770800a8df95c77a413236
thecode-master-sy <thecodemasster@gmail.com>
2023-06-03 07:41:11 +0100

    checking vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout({
             </FlexContainer>
 
             {children}
+            <Analytics/>
             <Footer />
           </ThemeContextProvider>
         </StyledComponentsRegistry>
